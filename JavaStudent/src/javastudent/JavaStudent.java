a7f5c0694f623a95f5fb19d2962c9bc9408d8456
owusukej <owusukej@sheridanc.on.ca>
2020-06-06 15:32:37 -0400

    New repo for StudentList

---
+++ JavaStudent/src/javastudent/JavaStudent.java
@@ -0,0 +1,16 @@
+
+package javastudent;
+
+/**
+ *
+ * @author Jimmy Kessie 
+ */
+public class JavaStudent {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+}
